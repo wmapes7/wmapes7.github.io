6d6c0db18a254b7e5117ac6669e6e5a8fdb4c15e
Will <wmapes@singlethread.com>
2018-06-26 15:03:22 -0400

    validation stuff

--- ResponsiveBuild/js/app.js
+++ ResponsiveBuild/js/app.js
@@ -6,41 +6,88 @@ $(function () {
 
 
 var $hoursWidget = '<div class="dropdown" id="hours" style="display:inline"><button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">Hours<span class="caret"></span></button><ul class="dropdown-menu" aria-labelledby="dropdownMenu1"><li><a href="#">.1</a></li><li><a href="#">.2</a></li><li><a href="#">.3</a></li><li><a href="#">.4</a></li><li><a href="#">.5</a></li><li><a href="#">.6</a></li><li><a href="#">.8</a></li><li><a href="#">.9</a></li><li><a href="#">1</a></li></ul></div>';
-var $okGlyph = '<span class="glyphicon glyphicon-ok" style="top: 0px; margin-right: 0px; color:#fff; border-bottom:0px;"></span>';
 
 //add a check mark to the selected square
 $( ".square" ).click(function() {
-  $($okGlyph).appendTo(this);
+  $(this).toggleClass('glyphicon glyphicon-ok');
 });
 
 //Mark tires section all-green
-$('#tires-section-green').click(function(i,obj) {
-  $('.square.material-green').toggleClass('glyphicon glyphicon-ok');
-  console.log('green tires');
-  $('#tires-panel').css('border-color','#4caf50');
-  $('#tires-heading').css('background-color','#4caf50');
+$('#tires-section-green').click(function() {
+  $(this).css('border','1px solid #fff');
+  console.log('added check mark');
+  $('#tires-panel').toggleClass('st-panel-complete-border');
+  console.log('added border');
+  $('#tires-heading').toggleClass('st-panel-complete-heading');
+  console.log('added panel heading green');
+  $(".material-green.square.tires-green").toggleClass('glyphicon glyphicon-ok');
+  console.log('added checks to green');
 });
 
-//check all green, turn panel border and panel heading green
+//Mark brakes section all-green
+$('#brakes-section-green').click(function() {
+  $(this).css('border','1px solid #fff');
+  console.log('added check mark');
+  $('#brakes-panel').toggleClass('st-panel-complete-border');
+  console.log('added border');
+  $('#brakes-heading').toggleClass('st-panel-complete-heading');
+  console.log('added panel heading green');
+  $(".material-green.square.brakes-green").toggleClass('glyphicon glyphicon-ok');
+  console.log('added checks to green');
+});
+
+//Mark fluids section all-green
+$('#fluids-section-green').click(function() {
+  $(this).css('border','1px solid #fff');
+  console.log('added check mark');
+  $('#fluids-panel').toggleClass('st-panel-complete-border');
+  console.log('added border');
+  $('#fluids-heading').toggleClass('st-panel-complete-heading');
+  console.log('added panel heading green');
+  $(".material-green.square.fluids-green").toggleClass('glyphicon glyphicon-ok');
+  console.log('added checks to green');
+});
+
+//Mark battery section all-green
+$('#battery-section-green').click(function() {
+  $(this).css('border','1px solid #fff');
+  console.log('added check mark');
+  $('#battery-panel').toggleClass('st-panel-complete-border');
+  console.log('added border');
+  $('#battery-heading').toggleClass('st-panel-complete-heading');
+  console.log('added panel heading green');
+  $(".material-green.square.battery-green").toggleClass('glyphicon glyphicon-ok');
+  console.log('added checks to green');
+});
+
+//Mark systems section all-green
+$('#systems-section-green').click(function() {
+  $(this).css('border','1px solid #fff');
+  console.log('added check mark');
+  $('#systems-panel').toggleClass('st-panel-complete-border');
+  console.log('added border');
+  $('#systems-heading').toggleClass('st-panel-complete-heading');
+  console.log('added panel heading green');
+  $('#systems-panel').find('.material-green').toggleClass('glyphicon glyphicon-ok');
+  console.log('added checks to green');
+});
+
+//check entire page green, turn panel border and panel heading green
 $('#all-green').click(function() {
-  $('.material-green').toggleClass("glyphicon glyphicon-ok");
-  $(this).toggleClass("glyphicon glyphicon-ok");
-  $('.panel').css('border-color','#4caf50');
-  $('.panel-heading').css('background-color','#4caf50');
-  $('#save').toggleClass('outline-btn').toggleClass('outline-btn-disabled');
-
-
+    $('span.square.material-green').toggleClass("glyphicon glyphicon-ok");
+    console.log('added checks');
+    $(this).toggleClass("glyphicon glyphicon-ok");
+    console.log('added checkmark to all green');
+    $('.st-panel-heading').toggleClass('st-panel-complete-heading');
+    console.log('toggled heading class');
+    //$('.panel').css('border-color','#4caf50');
+    $('.panel').toggleClass('st-panel-complete-border');
+    console.log('toggled panel border');
+    //$('.panel-heading').css('background-color','#4caf50');
+    $('#save').toggleClass('outline-btn').toggleClass('outline-btn-disabled');
   //$($okGlyph).appendTo('.material-green');
 
 });
-
-/*section green
-$('#tires-green').click(function(){
-  $('.tires-green').append($okGlyph);
-  $('#tires-panel').css('border-color','#4caf50');
-  $('#tires-heading').css('background-color','#4acf50');
-  $('#tires-heading').css('color','#fff');
-});*/
 
 //show hours widget after red click
 $('.st-red').click(function() {
@@ -53,14 +100,32 @@ $('.st-red').click(function() {
 
 //save button, section not completed
 $('#save').click(function(){
-  $('.panel-heading').each(function(i,obj){
+  $('.st-panel-heading').each(function(i,obj){
     console.log('panel'+ i);
     if ( !$(obj).hasClass('material-green')) {
       console.log('found incomplete');
       $('.panel').css('border-color','#fd265c');
-      $('.panel-heading').css('background-color','#fd265c');
+      $('.st-panel-heading').toggleClass('.st-panel-complete');
       $('html, body').animate ({scrollTop: 0},800);
       return false;
     }
+  });
+});
+
+//Check to see if an item has at least one check
+$('#test-button').click(function(){
+  console.log('button clicked');
+  $('.panel-body .col-sm-12 span:nth-last-of-type(4)').each(function(i,e){
+    console.log('selected spans');
+    if (!$('.st-panel-heading').hasClass('st-panel-complete-heading')) {
+      $('.panel').toggleClass('st-panel-incomplete-border');
+      console.log('toggled red border');
+      //description fields
+      $('.panel-body .col-sm-12 span:nth-last-of-type(1)').css('background-color','#ffc107')
+      console.log('yellow background description');
+      $('.st-panel-heading').toggleClass('st-panel-incomplete-heading');
+      console.log('toggled red heading');
+    };
+
   });
 });
